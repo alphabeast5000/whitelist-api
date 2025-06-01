705222b9e4d06237a374ca3545a1e1d95c88c45b
alphabeast5000 <totallynotrandomcrap@gmail.com>
2025-06-01 01:26:56 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const whitelist = [
+  298743938,
+  1632866443
+];
+
+const bannedgames = [
+  0,
+];
+
+app.get("/", (req, res) => {
+  res.send("WhitelistAPI is online!");
+});
+
+app.get("/check", (req, res) => {
+  const userId = parseInt(req.query.userId);
+  const gameId = parseInt(req.query.gameId);
+
+  const isWhitelisted = whitelist.includes(userId);
+  const isBannedGame = bannedgames.includes(gameId);
+
+  res.json({
+    allowed: isWhitelisted,
+    bannedgame: isBannedGame
+  });
+});
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
